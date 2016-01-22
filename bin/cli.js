5de987d07b38013583fb5cb23c9ec69c9ae939e1
Andrew Shaffer <andrew@weo.io>
2016-01-22 13:46:26 -0800

    add --modules option

--- bin/cli.js
+++ bin/cli.js
@@ -9,8 +9,6 @@ var path = require('path')
 var unv = require('..')
 var fs = require('fs')
 
-// Enable babel by default when used from the command line
-require('babel-register')
 
 /**
  * CLI
@@ -18,6 +16,12 @@ require('babel-register')
 
 var args = minimist(process.argv.slice(2))
 var cmd = args._[0]
+
+var modules = args.modules
+if (modules) configurePaths(path.resolve(process.cwd(), modules))
+
+// Enable babel by default when used from the command line
+require('babel-register')
 
 if (cmd === 'dev') {
   var client = args.client
@@ -37,6 +41,17 @@ if (cmd === 'dev') {
 /**
  * Helpers
  */
+
+/**
+ * Setup requires from your project's root
+ * source folder. So you can e.g., `require('components/modal')`
+ */
+
+function configurePaths (path) {
+  process.env.NODE_PATH = [process.env.NODE_PATH, path].filter(Boolean).join(':')
+  // XXX How bad of a hack is this?
+  require('module')._initPaths()
+}
 
 function tryDefaults (file) {
   if (fs.existsSync(path.join('src', file))) return path.join('src', file)
